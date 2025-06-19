d1de96285292f4a4356e20e14ea3e416ffd326a5
Ashley <142437333+MaybeAshleyIdk@users.noreply.github.com>
2025-06-19 19:24:50 +0200

    Sync slash commands with local commands on startup

--- bot/src/main/kotlin/io/github/maybeashleyidk/remotecontroldiscordbot/Bot.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/remotecontroldiscordbot/Bot.kt
@@ -4,10 +4,8 @@ import io.github.maybeashleyidk.remotecontroldiscordbot.localcommands.LocalComma
 import io.github.maybeashleyidk.remotecontroldiscordbot.localcommands.LocalCommandsConfig
 import io.github.maybeashleyidk.remotecontroldiscordbot.logging.Logger
 import io.github.maybeashleyidk.remotecontroldiscordbot.logging.Logger.Companion.logInfo
-import io.github.maybeashleyidk.remotecontroldiscordbot.utils.await
 import io.github.maybeashleyidk.remotecontroldiscordbot.utils.performGracefulShutdown
 import kotlinx.collections.immutable.ImmutableMap
-import kotlinx.collections.immutable.toImmutableMap
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
@@ -15,13 +13,6 @@ import net.dv8tion.jda.api.JDA as Jda
 import net.dv8tion.jda.api.JDABuilder as JdaBuilder
 import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.hooks.EventListener
-import net.dv8tion.jda.api.interactions.DiscordLocale
-import net.dv8tion.jda.api.interactions.IntegrationType
-import net.dv8tion.jda.api.interactions.InteractionContextType
-import net.dv8tion.jda.api.interactions.commands.Command
-import net.dv8tion.jda.api.interactions.commands.build.Commands
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
-import net.dv8tion.jda.api.requests.RestAction
 
 internal fun runBot(token: BotToken, localCommandsConfig: LocalCommandsConfig, logger: Logger) {
 	runBlocking {
@@ -31,11 +22,6 @@ internal fun runBot(token: BotToken, localCommandsConfig: LocalCommandsConfig, l
 
 private suspend fun runBotSuspending(token: BotToken, localCommandsConfig: LocalCommandsConfig, logger: Logger) {
 	coroutineScope {
-		val localCommands: List<LocalCommand> = localCommandsConfig.getCommandsAsList()
-
-		val slashCommandDataList: List<SlashCommandData> = localCommands
-			.map(LocalCommand::toSlashCommandData)
-
 		val deferredMainEventListenerConfig: CompletableDeferred<MainEventListener.Config> = CompletableDeferred()
 
 		MainEventListener(
@@ -54,21 +40,11 @@ private suspend fun runBotSuspending(token: BotToken, localCommandsConfig: Local
 
 			jda.awaitReady()
 
-			// TODO: Cache the slash command IDs instead of re-creating them every time.
-			logger.logInfo("Deleting all existing slash commands...")
-			jda.deleteAllCommands()
-			logger.logInfo("Successfully deleted all existing slash commands")
+			logger.logInfo("Syncing slash commands with local commands...")
+			val commandsMap: ImmutableMap<Long, LocalCommand> =
+				jda.syncSlashCommandsWithLocalCommands(localCommands = localCommandsConfig.commandsMap, logger)
+			logger.logInfo("Successfully synced slash commands with local commands")
 
-			val commandsMap: ImmutableMap<Long, LocalCommand> = jda.updateCommands()
-				.addCommands(slashCommandDataList)
-				.also { logger.logInfo("Creating slash commands...") }
-				.await()
-				.also { logger.logInfo("Successfully created slash commands") }
-				.withIndex()
-				.associate { (index: Int, command: Command) ->
-					command.idLong to localCommands[index]
-				}
-				.toImmutableMap()
 			val mainEventListenerConfig =
 				MainEventListener.Config(
 					authorizedUserIds = localCommandsConfig.authorizedUserIds,
@@ -81,35 +57,4 @@ private suspend fun runBotSuspending(token: BotToken, localCommandsConfig: Local
 			jda.awaitShutdown()
 		}
 	}
-}
-
-private fun LocalCommand.toSlashCommandData(): SlashCommandData {
-	val name: String = this.name.toString()
-
-	val commandLine: String = this.argv.toCommandLine()
-	val enGbDescription: String =
-		createEnGbSlashCommandDescription(uiStringResolver = UiStringResolver.English, commandLine = commandLine)
-	val enUsDescription: String =
-		createEnUsSlashCommandDescription(uiStringResolver = UiStringResolver.English, commandLine = commandLine)
-
-	return Commands.slash(name, enUsDescription)
-		.setIntegrationTypes(IntegrationType.USER_INSTALL)
-		.setContexts(InteractionContextType.ALL)
-		.setDescriptionLocalization(DiscordLocale.ENGLISH_UK, enGbDescription)
-		.setDescriptionLocalization(DiscordLocale.ENGLISH_US, enUsDescription)
-}
-
-private suspend fun Jda.deleteAllCommands() {
-	val commands: List<Command> = this.retrieveCommands().await()
-
-	if (commands.isEmpty()) {
-		return
-	}
-
-	commands
-		.map { command: Command ->
-			this.deleteCommandById(command.idLong)
-		}
-		.let { RestAction.allOf(it) }
-		.await()
 }
